8e6835753c9637133911bd01b279fe68810b9bc2
Samantha Le Huec <s.lhuec@gmail.com>
2023-01-30 16:18:19 +0100

    init constructors ebauche

--- containers/vector.hpp
+++ containers/vector.hpp
@@ -12,14 +12,35 @@ namespace ft {
 
 //______________Member type________________________________________________
 			
-			typedef T								value_type;
-			typedef Allocator						allocator_type;
-			typedef typename std::size_t			size_type;
-			typedef typename std::ptrdiff_t			difference_type;
-			typedef typename Allocator::pointer		pointer;
+		typedef T												value_type;
+		typedef Allocator										allocator_type;
+		typedef typename std::size_t							size_type;
+		typedef typename std::ptrdiff_t							difference_type;
+		typedef	T&												reference;
+		typedef	const T&										const_reference;
+		typedef typename Allocator::pointer						pointer;
+		typedef typename Allocator::const_pointer				const_pointer;
+		typedef T*												iterator;
+		typedef const T*										const_iterator;
+		typedef typename std::reverse_iterator<iterator>		reverse_iterator;
+		typedef typename std::reverse_iterator<const_iterator>	const_reverse_iterator;
 
-//______________Constructors and destructor_________________________________
-		Vector<T>() : 
+
+//______________Constructors and destructor_________________________________ 
+
+		// default construct empty container with default constructed allocator
+		Vector<T>() 
+
+		// fill constructor: construct a container with n lements each one is a copy of val
+		explicit Vector<T>(size_type n, const T& val = T(), const Allocator& alloc = Allocator())
+
+		// range constructor: construct a container with as many elem as the range (fist, last)
+		// with each elem contuct from its corresponding elem in that range in the same order
+		template <class InputIterator>  vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type())
+
+
+		~Vector<T>()
+
 	
 //______________Operator overload___________________________________________
 
@@ -36,7 +57,13 @@ namespace ft {
 
 		private:
 
-	}
+		Allocator	_alloc;
+		size_type	_n;
+		size_type	_capacity;
+		value_type	_val;
+		iterator	_first;
+
+	};
 }
 
 #endif
